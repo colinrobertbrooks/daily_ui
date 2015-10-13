110c48814899962a7e67a12ef8723c0e5797335f
Colin Cummings <colinrcummings@gmail.com>
2015-10-13 11:38:17 -0500

    Directory & 001 (sign up)

---
+++ js/001_sign_up.js
@@ -0,0 +1,58 @@
+$('#sign-up-form').formValidation({
+    framework: 'bootstrap',
+    fields: {
+        name: {
+            validators: {
+                notEmpty: {
+                    message: 'Please enter your name'
+                }
+            }
+        },
+        email: {
+            validators: {
+                notEmpty: {
+                    message: 'Please enter your email address'
+                },
+                emailAddress: {
+                    message: 'Please enter a valid email address'
+                }
+            }
+        },
+        password: {
+            validators: {
+                notEmpty: {
+                    message: 'Password cannot be blank'
+                },
+                stringLength: {
+                    min: 8,
+                }
+            }
+        },
+        confirmPassword: {
+            validators: {
+                notEmpty: {
+                    message: 'Password confirmation cannot be blank'
+                },
+                stringLength: {
+                    min: 8,
+                },
+                identical: {
+                    field: 'password',
+                    message: 'Password and password confirmation must match'
+                }
+            }
+        }
+    }
+})
+.on('err.form.fv', function(e) {
+    $('#sign-up-modal').addClass('shake animated');
+})
+.on('success.form.fv', function(e) {
+    e.preventDefault();
+    $('#sign-up-modal').modal('hide');
+    $('#sign-up-btn').hide();
+    $('#success-message')
+        .show()
+        .addClass('bounceInDown animated');
+});
+
