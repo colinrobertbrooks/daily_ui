dd2032d044b93eb0e98e40df65a9ea96948591a5
Colin Cummings <colinrcummings@gmail.com>
2015-10-16 12:18:36 -0500

    revise projects to challenges

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 //document ready function
 $( document ).ready(function(){
-  //load project data and draw  projects
-  d3.json('data/projects.json', function(data) {
-    drawProjects(data.projects);
+  //load challenge data and draw challenge elements
+  d3.json('data/challenges.json', function(data) {
+    drawChallenges(data.challenges);
   });
 });
 
-//TODO: VALIDATE PATH FOR
-//project function
-function drawProjects (data) {
+//challenge function
+function drawChallenges (data) {
   data.forEach(function(d){
-    $('#projects-hook').append(
-      '<div class="col-md-6 project">' +
-        '<div class="project-border">' +
+    $('#challenges-hook').append(
+      '<div class="col-md-6 challenge">' +
+        '<div class="challenge-border">' +
           '<h3>' + d.name + '</h3>' +
-          '<a href="' + d.link + '" title="View project ' + d.name + '">' +
-           '<img class="img-responsive center-block project-img" src="img/project_screenshots/' + d.image + '">' +
+          '<a href="' + d.link + '" title="View challenge ' + d.name + '">' +
+           '<img class="img-responsive center-block challenge-img" src="img/challenge_screenshots/' + d.image + '">' +
           '</a>' +
           '<p>' + d.description + '</p>' +
         '</div>' +
