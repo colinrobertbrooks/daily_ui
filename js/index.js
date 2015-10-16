9b45f69806aaf0def70763e919dc808292b979f2
Colin Cummings <colinrcummings@gmail.com>
2015-10-16 08:32:59 -0500

    update index

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 //document ready function
 $( document ).ready(function(){
   //load project data and draw  projects
-  d3.json('/data/projects.json', function(data) {
+  d3.json('data/projects.json', function(data) {
     drawProjects(data.projects);
   });
 });
 
+//TODO: VALIDATE PATH FOR
 //project function
 function drawProjects (data) {
   data.forEach(function(d){
@@ -14,7 +15,7 @@ function drawProjects (data) {
         '<div class="project-border">' +
           '<h3>' + d.name + '</h3>' +
           '<a href="' + d.link + '" title="View project ' + d.name + '">' +
-           '<img class="img-responsive center-block project-img" src="/img/project_screenshots/' + d.image + '">' +
+           '<img class="img-responsive center-block project-img" src="img/project_screenshots/' + d.image + '">' +
           '</a>' +
           '<p>' + d.description + '</p>' +
         '</div>' +
